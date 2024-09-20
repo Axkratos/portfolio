031466a1cdf227cda6be355acc8d4745d105d32d
Anup Adhikari <anup.211344@ncit.edu.np>
2024-09-20 16:06:57 +0545

    removed lorem

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { navLinksdata } from '../../constants';
+import { navLinksdata } from "../../constants";
 import resume from "../../assets/resume.pdf"; // Make sure this path points to your resume file
 
 const Navbar = () => {
@@ -11,12 +11,14 @@ const Navbar = () => {
 
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div className="flex items-center pl-4"> {/* Added padding-left to move Ad. right */}
-        <Link 
-          to="home" 
-          spy={true} 
-          smooth={true} 
-          offset={-70} 
+      <div className="flex items-center pl-4">
+        {" "}
+        {/* Added padding-left to move Ad. right */}
+        <Link
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-70}
           duration={500}
           className="text-3xl font-bold text-designColor cursor-pointer"
         >
@@ -61,11 +63,6 @@ const Navbar = () => {
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
                 <span className="text-xl font-bold text-designColor">Ad.</span>
-                <p className="text-sm text-gray-400 mt-2">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Earum soluta perspiciatis molestias enim cum repellat, magnam
-                  exercitationem distinctio aliquid nam.
-                </p>
               </div>
               <ul className="flex flex-col gap-4">
                 {navLinksdata.map((item) => (
@@ -92,13 +89,28 @@ const Navbar = () => {
                   Find me in
                 </h2>
                 <div className="flex gap-4">
-                  <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+                  <a
+                    href="https://www.facebook.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bannerIcon"
+                  >
                     <FaFacebookF />
                   </a>
-                  <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+                  <a
+                    href="https://twitter.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bannerIcon"
+                  >
                     <FaTwitter />
                   </a>
-                  <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+                  <a
+                    href="https://www.linkedin.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bannerIcon"
+                  >
                     <FaLinkedinIn />
                   </a>
                 </div>
@@ -115,6 +127,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
